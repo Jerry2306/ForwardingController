b08e6fdfc1903a1bd268e4686c703b334fa343b8
Jeremy Treutler <jeremy.treutler@prologis.de>
2021-06-14 18:33:58 +0200

    add throw notimplementedexception to console UI

--- src/ForwardingController/UserInterface/ForwardingControllerConsole/Program.cs
+++ src/ForwardingController/UserInterface/ForwardingControllerConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ForwardingControllerConsole
 {
@@ -10,6 +6,7 @@ namespace ForwardingControllerConsole
     {
         static void Main(string[] args)
         {
+            throw new NotImplementedException("Console interface not implemented!");
         }
     }
 }
