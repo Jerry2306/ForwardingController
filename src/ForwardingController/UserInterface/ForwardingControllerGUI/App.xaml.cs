97df08b21dbca5913575e665936de13113d491ce
Jeremy Treutler <jeremy.treutler@prologis.de>
2021-06-14 18:35:42 +0200

    add MainWindowViewModel and instancing via ninject kernel

--- src/ForwardingController/UserInterface/ForwardingControllerGUI/App.xaml.cs
+++ src/ForwardingController/UserInterface/ForwardingControllerGUI/App.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ForwardingControllerGUI.View;
+using ForwardingControllerGUI.ViewModel;
+using Ninject;
+using NinjectBindingManaging;
+using SharedItems.Extensions;
+using System;
+using System.Text;
 using System.Windows;
 
 namespace ForwardingControllerGUI
@@ -13,5 +14,29 @@ namespace ForwardingControllerGUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            try
+            {
+                var kernel = new NinjectBindingManager().GetKernel();
+
+                var window = new MainWindow
+                {
+                    DataContext = kernel.Get<MainWindowViewModel>()
+                };
+
+                window.Show();
+            }
+            catch (Exception exc)
+            {
+                var excBuilder = new StringBuilder();
+                excBuilder.AppendLine("Fatal error at loading main window:");
+                excBuilder.AppendLine(exc.GetFormattedMessage());
+                excBuilder.AppendLine($"Stacktrace: {exc.StackTrace}");
+                MessageBox.Show(excBuilder.ToString());
+            }
+        }
     }
 }
