6e5263d05c211a646fc437d20167d3ea1120592c
Jeremy Treutler <jeremy.treutler@prologis.de>
2021-06-14 18:34:43 +0200

    add cross cutting shared items

---
+++ src/ForwardingController/CrossCutting/SharedItems/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace SharedItems.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetFormattedMessage(this Exception source)
+        {
+            if (source == null)
+                return string.Empty;
+
+            var result = new StringBuilder();
+            result.AppendLine($"Message: {source.Message}");
+
+            var temp = source.InnerException;
+
+            for (var i = 1; temp != null; i++)
+            {
+                result.AppendLine($"Detail {i}: {temp.Message}");
+                temp = temp.InnerException;
+            }
+
+            return result.ToString();
+        }
+    }
+}
